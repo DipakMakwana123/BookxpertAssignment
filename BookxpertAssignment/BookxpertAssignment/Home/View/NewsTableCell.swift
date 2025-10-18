a4731030f37dcada5831707c29e3a0328c0a926c
Dipak Makwana <dipakmakwana123@gmail.com>
2025-10-18 12:16:10 +0530

    1)  News API Intergration 2) Design UITableView 3) Other many changes

---
+++ BookxpertAssignment/BookxpertAssignment/Home/View/NewsTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableCell.swift
+//  BookxpertAssignment
+//
+//  Created by Dipak Makwana on 17/10/25.
+//
+
+import UIKit
+
+class NewsTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ BookxpertAssignment/BookxpertAssignment/Home/ViewModel/NewsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewsViewModel.swift
+//  BookxpertAssignment
+//
+//  Created by Dipak Makwana on 17/10/25.
+//
+
+import Foundation
